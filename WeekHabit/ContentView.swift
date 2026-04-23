c4218a39e8e3f239fed4dcab91707b0955927616
valentinodepaola <v.depaolagallardo@gmail.com>
2026-04-22 19:42:19 -0600

    Initial commit

---
+++ WeekHabit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeekHabit
+//
+//  Created by Valentino De Paola Gallardo on 22/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeekHabit/WeekHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekHabitApp.swift
+//  WeekHabit
+//
+//  Created by Valentino De Paola Gallardo on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WeekHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
